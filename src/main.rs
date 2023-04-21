6b417cc2be6138222eaf37e50b814cd2dfe581f3
borisdimitrov1998@gmail.com <borisdimitrov1998@gmail.com>
2023-04-21 19:11:10 +0300

    indend fix

--- src/main.rs
+++ src/main.rs
@@ -34,36 +34,36 @@ fn create_excel() {
 
     let sw = sheet_writer;
 
-    // Define connection parameters
-    let server = "localhost";
-    let database = "test-db";
-    let username = "root";
-    let password = "";
-    let port = 3306; // 3306 otherwise 3307 for ssh tunnel
+        // Define connection parameters
+        let server = "localhost";
+        let database = "test-db";
+        let username = "root";
+        let password = "";
+        let port = 3306; // 3306 otherwise 3307 for ssh tunnel
 
-    // Build options for the database connection
-    let opts = OptsBuilder::new()
-        .ip_or_hostname(Some(server))
-        .db_name(Some(database))
-        .user(Some(username))
-        .pass(Some(password))
-        .tcp_port(port)
-        .ssl_opts(None);
+        // Build options for the database connection
+        let opts = OptsBuilder::new()
+            .ip_or_hostname(Some(server))
+            .db_name(Some(database))
+            .user(Some(username))
+            .pass(Some(password))
+            .tcp_port(port)
+            .ssl_opts(None);
 
-    // Connect to the database
-    let pool = mysql::Pool::new(mysql::Opts::from(opts)).unwrap();
-    let mut conn = pool.get_conn().unwrap();
+        // Connect to the database
+        let pool = mysql::Pool::new(mysql::Opts::from(opts)).unwrap();
+        let mut conn = pool.get_conn().unwrap();
 
-    let result: Vec<(String, String, String, String, String)> = conn
-        .query("SELECT id, client, quarter, year, wages FROM taxable_wages")
-        .unwrap();
+        let result: Vec<(String, String, String, String, String)> = conn
+            .query("SELECT id, client, quarter, year, wages FROM taxable_wages")
+            .unwrap();
 
-    for db_row in result {
-        sw.append_row(row![db_row.0, db_row.1, db_row.2, db_row.3, db_row.4])?;
-    }
+        for db_row in result {
+            sw.append_row(row![db_row.0, db_row.1, db_row.2, db_row.3, db_row.4])?;
+        }
 
 
-    sw.append_row(row!["END", "LINE", true])
+        sw.append_row(row!["END", "LINE", true])
 
     }).expect("write excel error!");
 
